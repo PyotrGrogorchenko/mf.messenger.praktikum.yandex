49f3407ce6c0b554c396cc7a3743c42570c4add5
PyotrPetrovich <PePeGrigorchenko@yandex.ru>
2021-01-19 15:25:35 +0300

    webpack

--- app.js
+++ app.js
@@ -3,6 +3,8 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const PORT = 4000;
+// const resolveCwd = require('resolve-cwd')
+// console.log(resolveCwd)
 app.use(express.static(path.join(__dirname, 'static')));
 app.get('/', function (request, res) {
     res.status(200).sendFile('index.html', { root: path.join(__dirname, './static') });
---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+// const path = require('path')
+
+// module.exports = {
+//   entry: './static/js/index.js',
+//   output: {
+//     filename: 'bundle.js',
+//     path: path.resolve(__dirname, 'static/dist')
+//     //path: 'static/dist'
+//   }
+// }
